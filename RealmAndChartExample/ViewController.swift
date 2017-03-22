0093994ae59223bf625010c7f9580f27a6a24e1b
sassi walid <sassi.walid@outlook.com>
2017-03-22 19:59:06 +0100

    Initial Commit

---
+++ RealmAndChartExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmAndChartExample
+//
+//  Created by Walid Sassi on 22/03/2017.
+//  Copyright © 2017 Walid Sassi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
